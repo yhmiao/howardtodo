a41f948e82dbced0da84f9676e6bdb8d88c09531
Howard <boredassian@yahoo.com>
2018-05-12 14:03:03 +0800

    Setup devise gem and devise user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  devise_for :users
 end
